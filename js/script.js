35917112c12cc21bee6d1fb70670dc9a40c9e4b4
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-03-03 16:58:00 +0100

    L'esercizio "Pari e Dispari" js

--- js/script.js
+++ js/script.js
@@ -18,5 +18,27 @@ console.log(result);
 if (result === userWord) {
     console.log(`La parola "${userWord}" è palindroma`);
 } else {
-    console.log(`La parola "${userWord}" non è palidroma`);
+    console.log(`La parola "${userWord}" non è palindroma`);
 }
+
+
+/* Pari e Dispari */
+
+const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
+console.log(userNumber);
+
+const computerNumber = Math.floor((Math.random() * 5) + 1);
+console.log(computerNumber);
+
+const sum = userNumber + computerNumber;
+console.log(sum);
+
+function isEven(number) {
+    if(number % 2 == 0) {
+        console.log('Il numero è pari: ho vinto io!');
+    } else {
+        console.log('Il numero è dispari: hai vinto!');
+    }
+};
+
+const resultSum = isEven(sum);
